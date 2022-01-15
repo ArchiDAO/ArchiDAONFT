43cd15e2e132d3d8bdf0b0e968fd943ff248aa27
arlav <dounas@gmail.com>
2022-01-15 15:01:48 +0000

    Update SNFT.sol

--- contracts/SNFT.sol
+++ contracts/SNFT.sol
@@ -1,2 +1,94 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
+
+//@dev generated from OZ wizzard.
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+//
+//  string Name
+//  string email
+//  string bio
+
+struct Skill {
+  address owner;
+  uint256 architecture;
+  uint256 interior;
+  uint256 urban;
+
+}
+
+Skill skill;
+address userAddress;
+
+
+    constructor() ERC721("Skills NFT", "SNF") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+
+    function setSkill(address _userAddress, uint256 _architecture, uint256 _interior, uint256 _urban) public onlyOwner {
+      skill Storage Skill = Skill(userAddress,architecture,interior,urban);
+
+
+    }
+
+//    function getSkills()
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
