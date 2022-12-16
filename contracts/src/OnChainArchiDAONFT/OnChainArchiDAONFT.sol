68f246ae61d9a1d87f00dbe44a0c0076c9d14bbc
Hico <70553434+HicoMcD@users.noreply.github.com>
2022-12-16 17:34:03 +0200

    Implemented: non-tranferable, remove whitelist

--- contracts/src/OnChainArchiDAONFT/OnChainArchiDAONFT.sol
+++ contracts/src/OnChainArchiDAONFT/OnChainArchiDAONFT.sol
@@ -4,26 +4,18 @@ pragma solidity ^0.8.16;
 // import "hardhat/console.sol";
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 //check snapshot for voting
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 // import {Base64} from "./Base64.sol";
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
-
-// 8/12/22
-//Polygon deployment (DID's), to early
-
-
-contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
+contract OnChainArchiDAONFT is ERC721, Ownable, ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     Counters.Counter private _memberIds;
-
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     // PNG image URI on IPFS, can update
     string public imageIPFSFolderURI = 'https://ipfs.io/ipfs/QmRHgykzBUuQR4FuWUH2mtpabWagLSuxndjD3fK2c7ZJ89/';
@@ -31,7 +23,6 @@ contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
 //need to have NFT to access to ERC20 tokens to be able to stake, be part of the project
     struct MemberSkills {
         uint256 memberId;
-        uint256 projectsCompleted; //mapping(uint => struct) projectDetails
         uint256 skill_1; //engineering : value 0 - 100 max.
         uint256 skill_2; //design
         uint256 skill_3; //registration
@@ -42,10 +33,22 @@ contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
         uint256 skill_8;
         uint256 skill_9;
         uint256 skill_10;
+        uint256 projectsCompleted; 
     }
-    //mapping to save member structs
+    //MemberSkills Struct mapping
     mapping (address => MemberSkills) public memberSkillsStructMap;
-    // MemberSkills[] public memberSkills;
+
+    // struct ProjectDetail {
+    //     string projectName;
+    //     string projectDescription;
+    //     string projectTimeline;
+    //     string projectCost;
+    //     uint256 skillPointsGained1;
+    //     uint256 skillPointsGained2;
+    //     uint256 skillPointsGained3;
+    // }
+    // // ProjectDetail struct mapping
+    // mapping(address => (mapping(uint) => ProjectDetail)) projectDetails;
 
     //mapping for addresses to NFTS
     mapping (address => uint256) public addressToNFTNumber;
@@ -53,41 +56,49 @@ contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
     //whitelist member
     mapping (address => bool) public whitelistedMember;
 
+    // Used to make th eNFT non-transferable
+    bool public isTokenTransferable = false;
+
     constructor() ERC721 ("ArchiDAO Skills NFT", "ARCH") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+
     }
 
-    //whitelist member address and create memberSkills struct
-    function whitelistMember(address[] memory _memberAddress) public {
-        //require owner/access control to submit address (possibly do batch whitelisting)
+    //whitelist member address to enable minting
+    function whitelistMember(address[] memory _memberAddress) public onlyOwner {
+        // require(!whitelistedMember[_memberAddress], "Member already whitelisted");
+        //require: integrate other requirements needed before address whitelisted (referral)
 
         //Batch whitelisting
         for(uint256 i = 0; i < _memberAddress.length; i++ ) {
-            _memberIds.increment();
-            uint256 currentMemberId = _memberIds.current();
-
-            memberSkillsStructMap[_memberAddress[i]] = MemberSkills(currentMemberId, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
             whitelistedMember[_memberAddress[i]] = true;
         }
-
     }
 
+    // Remove whitelisted member that hasnt minted a token yet
     function removeWhitelistedMember(address _memberAddress) public {
+        require(whitelistedMember[_memberAddress], "Member not whitelisted");
+        require(balanceOf(_memberAddress) < 1, "Member already minted NFT");
 
+        whitelistedMember[_memberAddress] = false;
     }
 
     //Minting NFT function
     function mint() public /*onlyRole(MINTER_ROLE)*/ {
-        //require whitelistedMember to mint
+        // Address must be whitelisted to be able ot mint
         // require(whitelistedMember[msg.sender], "Not whitelisted");
 
-        //require can only mint 1 NFT (NFT's connected to addresses)
+        // Each Address can only mint 1 NFT
         require(addressToNFTNumber[msg.sender] == 0, "Already minted NFT");
+
+        //require: Additional requirements before being able to mint (time, cost)
+        //IE must be whitelisted for certain amount of time and complete tasks
 
         _tokenIds.increment();
         uint256 currentTokenId = _tokenIds.current();
+
+        memberSkillsStructMap[msg.sender] = MemberSkills(currentTokenId, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        // Initialise ProjectDetail struct with mapping projectDetails
 
         addressToNFTNumber[msg.sender] = currentTokenId;
 
@@ -169,7 +180,6 @@ contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
         string memory memberSkillLevel_9 = memberSkillsStruct.skill_9.toString();
         string memory memberSkillLevel_10 = memberSkillsStruct.skill_10.toString();
 
-
         bytes memory dataURI = abi.encodePacked(
                 '"skill_6": "', memberSkillLevel_6, '",', 
                 '"skill_7": "', memberSkillLevel_7, '",', 
@@ -184,35 +194,36 @@ contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
         );
     }
 
-    function getNestedProjectsMetadata(uint tokenId) internal view returns (string memory) {}
-
     function updateIPFSImageFolderURI (string memory newIPFSURI) public {
         imageIPFSFolderURI = newIPFSURI;
     }
 
-// CONTINUE FROM HERE /////
     //Update memberSkillsStruc with increased skill level
-    function updateMemberSkills(uint tokenId, bool skill_1, bool skill_2) public {
-        // require tokenexsists or membership exists
+    function updateMemberSkills(uint tokenId, uint _skill_1, uint _skill_2) public {
+        // require token exists
+        // onlyOwner (multisig)
 
         address memberSkillsToUpdate = ownerOf(tokenId);
 
         MemberSkills storage memberSkillsStruct = memberSkillsStructMap[memberSkillsToUpdate];
 
-        memberSkillsStruct.projectsCompleted++; //= memberSkillsStruct.projectsCompleted + 1;
+        memberSkillsStruct.projectsCompleted++; 
 
-        if(skill_1 == true) {
-            memberSkillsStruct.skill_1++;
-        }
-
-        if(skill_2 == true) {
-            memberSkillsStruct.skill_2++;
-        }
+        memberSkillsStruct.skill_1 = _skill_1;
+        memberSkillsStruct.skill_2 = _skill_2;
+        // memberSkillsStruct.skill_2 = _skill_3;
+        // memberSkillsStruct.skill_2 = _skill_4;
+        // memberSkillsStruct.skill_2 = _skill_5;
+        // memberSkillsStruct.skill_2 = _skill_6;
+        // memberSkillsStruct.skill_2 = _skill_7;
+        // memberSkillsStruct.skill_2 = _skill_8;
+        // memberSkillsStruct.skill_2 = _skill_9;
+        // memberSkillsStruct.skill_2 = _skill_10;
 
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
-    // The following functions are overrides required by Solidity.
 
+    // The following functions are overrides required by Solidity.
     function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         override(ERC721)
@@ -236,33 +247,20 @@ contract OnChainArchiDAONFT is ERC721, AccessControl, ERC721URIStorage {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, AccessControl)
+        override(ERC721)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) 
+        public 
+        override(ERC721)
+    {
+    // Check if the NFT is transferable
+    require(isTokenTransferable, "The NFT is soulbound and not transferable");
+
+    // Perform the transfer as normal
+    return super.transferFrom(_from, _to, _tokenId);
+    }
 }
-
-    // function getLevels(uint256 tokenId) public view returns (string memory) {
-    //     uint256 levels = tokenIdToMemberId[tokenId];
-    //     return levels.toString();
-    // }
-
-    // function train(uint256 tokenId) public {
-    //     require(_exists(tokenId), "Please use an existing token");
-    //     require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
-    //     uint256 currentLevel = tokenIdToMemberId[tokenId];
-    //     tokenIdToMemberId[tokenId] = currentLevel + 1;
-    //     _setTokenURI(tokenId, getTokenURI(tokenId));
-    // }
-
-    // function updateSkill1(uint256 tokenId) public returns (string memory) {
-    //     require(_exists(tokenId), "Please use an existing token");
-    //     require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
-
-    //     uint256 currentSkillLevel = tokenIdToSkill1[tokenId];
-    //     tokenIdToSkill1[tokenId] = currentSkillLevel + 1;
-    //     _setTokenURI(tokenId, getTokenURI(tokenId));
-    //     return tokenIdToSkill1[tokenId].toString();
-    // }
-
